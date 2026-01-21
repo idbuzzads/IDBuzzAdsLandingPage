1f414b0b913e602234c2a468055f65283cde1dfe
idbuzzads <idbuzzads@gmail.com>
2026-01-21 05:38:47 -0700

    Updated UploadPreview.tsx

--- src/components/UploadPreview.tsx
+++ src/components/UploadPreview.tsx
@@ -15,7 +15,6 @@ export default function UploadPreview({
   previewImage,
   onClearImage,
 }: UploadPreviewProps) {
-  const [dragActive, setDragActive] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleDrag = (e: React.DragEvent) => {
